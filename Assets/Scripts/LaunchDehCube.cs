1498e62dedfd27a8fe261597e6ccfb515a4cf5c1
jb1361 <justinbutler4@hotmail.com>
2018-08-16 19:06:16 -0400

    Add assets

---
+++ Assets/Scripts/LaunchDehCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchDehCube : MonoBehaviour {
+
+    public GameObject _cube;
+    public int velocity = 1;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        _cube.transform.position += _cube.transform.forward * Time.deltaTime * velocity;
+    }
+}
