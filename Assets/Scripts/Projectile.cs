b7e366371bae5a079ac0948fb643d79890d32c5d
jb1361 <justinbutler4@hotmail.com>
2018-08-16 19:28:00 -0400

    Encapulate projectile script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaunchDehCube : MonoBehaviour {
+public class Projectile : MonoBehaviour {
+
 
     public GameObject _cube;
     public int velocity = 1;
-	// Use this for initialization
-	void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
 		
 	}
-
+	
     void FixedUpdate()
     {
         _cube.transform.position += _cube.transform.forward * Time.deltaTime * velocity;
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "weapon") Destroy(this.gameObject);
+    }
+
+   
 }
