29ecad96542e175b8ecb4275e92d8d64e7d0242d
jb1361 <justinbutler4@hotmail.com>
2018-08-16 20:51:22 -0400

    Add active side color and destruction mechanic

--- Assets/Scripts/Projectile Scripts/ChildColliderController.cs
+++ Assets/Scripts/Projectile Scripts/ChildColliderController.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class ChildColliderController : MonoBehaviour {
 
+   // GameObject parentObject;
+    Projectile parentComponent;
+
+    //Sets parent object and parent component
+    public void SetParentObject(GameObject _parent)
+    {
+       // parentObject = _parent;
+        parentComponent = _parent.GetComponent<Projectile>();
+    }
+    public void SetActive()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.gameObject.tag == "weapon") transform.parent.GetComponent<Projectile>().CollisionDetected(this);
+        //We check to make sure only a weapon can destroy the projectile
+        if(collision.transform.gameObject.tag == "weapon") parentComponent.CollisionDetected(this);
     }
 }
--- Assets/Scripts/Projectile Scripts/Projectile.cs
+++ Assets/Scripts/Projectile Scripts/Projectile.cs
@@ -10,22 +10,27 @@ public class Projectile : MonoBehaviour {
 
     public enum AcceptedCollision{
         TOP,
-        FRONT,
         BOTTOM,
+        FRONT,
+        BACK,
         LEFT,
         RIGHT
     }
     public AcceptedCollision CollisionSide;
 
-    GameObject activeSide;
+    // GameObject activeSide;
 
-    GameObject[] children = new GameObject[5];
+   // GameObject[] children = new GameObject[5];
 
     // Use this for initialization
     void Start () {
+        //We iterate through the children and setup the child objects components propertys
         foreach (Transform child in projectile.transform)
-        {         
-            getChildren(child.gameObject);
+        {
+            // getChildren(child.gameObject);
+            ChildColliderController childComponent = child.GetComponent<ChildColliderController>();
+            childComponent.SetParentObject(projectile);
+            if(child.gameObject.tag == "CUBE_" + CollisionSide.ToString()) childComponent.SetActive();
         }
         
 	}
@@ -35,6 +40,7 @@ public class Projectile : MonoBehaviour {
         projectile.transform.position += projectile.transform.forward * Time.deltaTime * velocity;
     }
 
+    /*
     void getChildren(GameObject child)
     {
         for(int i = 0; i < children.Length; i++)
@@ -42,18 +48,13 @@ public class Projectile : MonoBehaviour {
             if (!children[i]) children[i] = child;
         }
     }
+    */
+    bool canBeDestroyed = true;
 
     public void CollisionDetected(ChildColliderController childScript)
     {
-        if (childScript.gameObject.tag == "CUBE_" + CollisionSide.ToString())
-        {
-            Debug.Log("right side");
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Debug.Log("wrong side");          
-        }
+        if (childScript.gameObject.tag == "CUBE_" + CollisionSide.ToString() && canBeDestroyed) Destroy(this.gameObject);
+        else canBeDestroyed = false; 
     }
 
 }
